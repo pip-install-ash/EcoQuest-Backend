5b7a909b1edc019e73d56dcd4b2dfe0cf23c2ae1
Ash <aklworld365@gmail.com>
2024-11-29 23:26:21 +0500

    add cors functionality.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-const express = require("express");
-const admin = require("firebase-admin");
-const serviceAccount = require("./keys.json");
-const assetRoutes = require("./routes/assets");
-const pointsRoutes = require("./routes/points");
-const leagueRoutes = require("./routes/league");
-const userRoutes = require("./routes/users");
-const checkAuth = require("./middleware/authentication");
+const express = require('express');
+const admin = require('firebase-admin');
+const serviceAccount = require('./keys.json');
+const assetRoutes = require('./routes/assets');
+const pointsRoutes = require('./routes/points');
+const leagueRoutes = require('./routes/league');
+const userRoutes = require('./routes/users');
+const cors = require('cors');
+
+const checkAuth = require('./middleware/authentication');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -14,19 +16,25 @@ admin.initializeApp({
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Firebase Authentication and Post Management!");
+app.get('/', (req, res) => {
+  res.send('Welcome to the Firebase Authentication and Post Management!');
 });
 
 // Register new user
-app.post("/register", async (req, res) => {
+app.post('/register', async (req, res) => {
   const { userName, email, password } = req.body;
 
   if (!userName || !email || !password) {
     return res.status(400).json({
-      message: "Username, email and password are required",
+      message: 'Username, email and password are required',
       success: false,
     });
   }
@@ -38,10 +46,10 @@ app.post("/register", async (req, res) => {
       password,
     });
 
-    console.log("Successfully created user:", userRecord.uid);
+    console.log('Successfully created user:', userRecord.uid);
 
     // Add user profile to Firestore in the userProfiles collection
-    await admin.firestore().collection("userProfiles").doc(userRecord.uid).set({
+    await admin.firestore().collection('userProfiles').doc(userRecord.uid).set({
       userID: userRecord.uid,
       userName,
       email,
@@ -49,23 +57,23 @@ app.post("/register", async (req, res) => {
     });
 
     return res.status(201).json({
-      message: "User registered and profile created successfully",
+      message: 'User registered and profile created successfully',
       success: true,
     });
   } catch (error) {
-    console.error("Error creating user or profile:", error);
+    console.error('Error creating user or profile:', error);
     return res.status(500).json({ message: error.message, success: false });
   }
 });
 
 // Log in a user
-app.post("/login", (req, res) => {
+app.post('/login', (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
     return res
       .status(500)
-      .json({ message: "Email and password are required", success: false });
+      .json({ message: 'Email and password are required', success: false });
   }
 
   admin
@@ -75,41 +83,41 @@ app.post("/login", (req, res) => {
       if (password === userRecord.providerData[0].providerId) {
         req.session.user = userRecord.uid;
         return res.json({
-          message: "User Login successful",
+          message: 'User Login successful',
           success: true,
         });
       } else {
         return res
           .status(401)
-          .json({ message: "Login failed", success: false });
+          .json({ message: 'Login failed', success: false });
       }
     })
     .catch((error) => {
-      console.error("Error getting user:", error);
-      if (error.code === "auth/user-not-found") {
+      console.error('Error getting user:', error);
+      if (error.code === 'auth/user-not-found') {
         res.status(404).json({
           message: "User doesn't exist. Please register first",
           success: false,
         });
       } else {
-        res.status(401).json({ message: "Login failed", success: false });
+        res.status(401).json({ message: 'Login failed', success: false });
       }
     });
 });
 
 // Log out a user
-app.get("/logout", (req, res) => {
+app.get('/logout', (req, res) => {
   req.session.user = null;
-  res.json({ message: "Logged out", success: true });
+  res.json({ message: 'Logged out', success: true });
 });
 
-app.use("/api", assetRoutes);
-app.use("/api/points", pointsRoutes);
-app.use("/api/leagues", leagueRoutes);
-app.use("/api/users", userRoutes);
+app.use('/api', assetRoutes);
+app.use('/api/points', pointsRoutes);
+app.use('/api/leagues', leagueRoutes);
+app.use('/api/users', userRoutes);
 
 // Create a new asset with additional data (requires authentication)
-app.post("/buildings/new", checkAuth, (req, res) => {
+app.post('/buildings/new', checkAuth, (req, res) => {
   const { buildingId, isCreated, isForbidden, isDestroyed, isRotate, x, y } =
     req.body;
 
@@ -127,7 +135,7 @@ app.post("/buildings/new", checkAuth, (req, res) => {
   // }
 
   const db = admin.firestore();
-  const buildingRef = db.collection("buildings").doc();
+  const buildingRef = db.collection('buildings').doc();
 
   const data = {
     createdAt: admin.firestore.FieldValue.serverTimestamp(),
@@ -144,10 +152,10 @@ app.post("/buildings/new", checkAuth, (req, res) => {
   buildingRef
     .set(data)
     .then(() => {
-      return res.json({ message: "Asset created", success: true });
+      return res.json({ message: 'Asset created', success: true });
     })
     .catch((error) => {
-      console.error("Error creating asset:", error);
+      console.error('Error creating asset:', error);
       return res.status(500).json({ message: error.message, success: false });
     });
 });
