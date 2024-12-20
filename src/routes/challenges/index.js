5cec8e451cc9596f7db07ccd819aea83b3a669e7
Ash <aklworld365@gmail.com>
2024-12-21 04:45:19 +0500

    Complete challenges functionality + notifications for challenges added

--- src/routes/challenges/index.js
+++ src/routes/challenges/index.js
@@ -6,17 +6,16 @@ const createResponse = require("../../utils/helper-functions");
 const router = express.Router();
 
 const challenges = [
-  { id: 1, description: "Upgrade 2 Houses", progress: "1/2", reward: 200 },
   {
-    id: 2,
+    id: 1,
     description: "Build 4 Residential houses",
     progress: "1/4",
     reward: 200,
   },
-  { id: 3, description: "Build a Factory", progress: "0/1", reward: 200 },
-  { id: 4, description: "Build a School", progress: "0/1", reward: 200 },
-  { id: 5, description: "Build a Hospital", progress: "0/1", reward: 200 },
-  { id: 6, description: "Build two Windmills", progress: "0/2", reward: 200 },
+  { id: 2, description: "Build a Factory", progress: "0/1", reward: 200 },
+  { id: 3, description: "Build a School", progress: "0/1", reward: 200 },
+  { id: 4, description: "Build a Hospital", progress: "0/1", reward: 200 },
+  { id: 5, description: "Build two Windmills", progress: "0/2", reward: 200 },
 ];
 
 router.get("/random-challenge", checkAuth, (req, res) => {
@@ -31,111 +30,402 @@ router.get("/random-challenge", checkAuth, (req, res) => {
   );
 });
 
-const disasters = ["earthquake", "flood", "fire outbreak", "hurricane"];
+router.post("/create-challenge", checkAuth, async (req, res) => {
+  const { startTime, endTime, leagueID, message, required, points } = req.body;
 
-router.get("/random-disaster", checkAuth, async (req, res) => {
+  if (
+    !startTime ||
+    !endTime ||
+    !message ||
+    !required ||
+    typeof required !== "object" ||
+    !required.buildingID ||
+    !required.count ||
+    !points
+  ) {
+    return res
+      .status(400)
+      .json(createResponse(false, "All fields are required"));
+  }
+
+  const newChallenge = {
+    startTime,
+    endTime,
+    leagueID: leagueID || null,
+    message,
+    required,
+    points,
+    isEnded: false,
+  };
+
   try {
-    // Fetch all user assets from Firestore
-    const userAssetsSnapshot = await admin
-      .firestore()
-      .collection("userAssets")
-      .get();
-    const userAssets = userAssetsSnapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
-
-    // Select 3 random assets from the user assets
-    const randomAssets = [];
-    for (let i = 0; i < 3; i++) {
-      const randomIndex = Math.floor(Math.random() * userAssets.length);
-      randomAssets.push(userAssets.splice(randomIndex, 1)[0]);
+    if (leagueID) {
+      const leagueRef = admin.firestore().collection("leagues").doc(leagueID);
+      const leagueDoc = await leagueRef.get();
+      if (!leagueDoc.exists) {
+        return res
+          .status(404)
+          .json(
+            createResponse(false, `No league found for the id: ${leagueID}`)
+          );
+      }
     }
 
-    // Mark the selected assets as destroyed in Firestore
-    const batch = admin.firestore().batch();
-    randomAssets.forEach((asset) => {
-      const assetRef = admin.firestore().collection("userAssets").doc(asset.id);
-      batch.update(assetRef, { isDestroyed: true });
-    });
-    await batch.commit();
-
-    // Extract building IDs and userIds from the destroyed assets
-    const buildingIds = randomAssets
-      .map((asset) => String(asset.buildingId)) // Convert to string
-      .filter((id) => typeof id === "string" && id.trim() !== "");
-
-    console.log("Building IDs:", buildingIds);
-
-    // If no valid building IDs are found, return an error response
-    if (buildingIds.length === 0) {
-      return res
-        .status(400)
-        .json(createResponse(false, "No valid building IDs found"));
-    }
-
-    // Fetch building details from Firestore using the building IDs
-    const buildingsSnapshot = await admin
+    const buildingRef = admin
       .firestore()
       .collection("buildings")
-      .where(admin.firestore.FieldPath.documentId(), "in", buildingIds)
+      .doc(required.buildingID);
+    const buildingDoc = await buildingRef.get();
+    if (!buildingDoc.exists) {
+      return res
+        .status(404)
+        .json(
+          createResponse(
+            false,
+            `No building found for the id: ${required.buildingID}`
+          )
+        );
+    }
+
+    const challengeRef = await admin
+      .firestore()
+      .collection("challenges")
+      .add(newChallenge);
+
+    // Create the notification document
+    const notificationDoc = {
+      message:
+        "New eco challenge! Complete the challenge to get 200 coins reward",
+      notificationType: "challenge",
+      isGlobal: true,
+      userID: null,
+    };
+    await admin.firestore().collection("notifications").add(notificationDoc);
+
+    res.json(
+      createResponse(true, "Challenge created successfully", {
+        id: challengeRef.id,
+      })
+    );
+  } catch (error) {
+    console.error("Error creating challenge:", error);
+    res
+      .status(500)
+      .json(createResponse(false, "An error occurred", error.message));
+  }
+});
+
+router.post("/test-building-creation", checkAuth, async (req, res) => {
+  const { buildingID, userID, leagueID } = req.body;
+
+  if (!buildingID || !userID) {
+    return res
+      .status(400)
+      .json(createResponse(false, "buildingID and userID are required"));
+  }
+
+  try {
+    await handleBuildingCreation(buildingID, userID, leagueID);
+    res.json(createResponse(true, "Building creation handled successfully"));
+  } catch (error) {
+    console.error("Error handling building creation:", error);
+    res
+      .status(500)
+      .json(createResponse(false, "An error occurred", error.message));
+  }
+});
+
+async function handleBuildingCreation(buildingID, userID, leagueID = null) {
+  try {
+    console.log("Starting handleBuildingCreation function");
+    console.log(
+      "buildingID:",
+      buildingID,
+      "userID:",
+      userID,
+      "leagueID:",
+      leagueID
+    );
+
+    // Get all challenges with isEnded: false
+    const challengesSnapshot = await admin
+      .firestore()
+      .collection("challenges")
+      .where("isEnded", "==", false)
       .get();
-    const buildings = buildingsSnapshot.docs.map((doc) => ({
+    const challenges = challengesSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    console.log("Fetched challenges:", challenges);
+
+    // Update isEnded to true for challenges where endTime has passed
+    const now = new Date();
+    const batch = admin.firestore().batch();
+    challenges.forEach((challenge) => {
+      if (new Date(challenge.endTime) < now) {
+        const challengeRef = admin
+          .firestore()
+          .collection("challenges")
+          .doc(challenge.id);
+        batch.update(challengeRef, { isEnded: true });
+      }
+    });
+    await batch.commit();
+    console.log("Updated ended challenges");
+
+    // Get active challenges with the provided buildingID
+    const activeChallengesSnapshot = await admin
+      .firestore()
+      .collection("challenges")
+      .where("isEnded", "==", false)
+      .where("required.buildingID", "==", buildingID)
+      .get();
+    const activeChallenges = activeChallengesSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    console.log("Fetched active challenges:", activeChallenges);
+
+    if (activeChallenges.length === 0) {
+      console.log("No active challenges found for the provided buildingID.");
+      return { success: true, message: "No active challenges found" };
+    }
+
+    // Process each active challenge
+    for (const challenge of activeChallenges) {
+      const challengeID = challenge.id;
+      const requiredCount = challenge.required.count;
+      console.log("Processing challenge:", challengeID);
+
+      // Get the challengeProgress document for the provided userID and challengeID
+      const challengeProgressSnapshot = await admin
+        .firestore()
+        .collection("challengeProgress")
+        .where("userID", "==", userID)
+        .where("challengeID", "==", challengeID)
+        .where("leagueID", "==", leagueID || null)
+        .get();
+
+      let challengeProgressDoc;
+      if (challengeProgressSnapshot.empty) {
+        // Create a new challengeProgress document
+        challengeProgressDoc = {
+          challengeID,
+          userID,
+          leagueID: leagueID || null,
+          isCompleted: requiredCount === 1,
+          progress: {
+            buildingID,
+            count: 1,
+          },
+        };
+        await admin
+          .firestore()
+          .collection("challengeProgress")
+          .add(challengeProgressDoc);
+        console.log(
+          "Created new challengeProgress document:",
+          challengeProgressDoc
+        );
+      } else {
+        // Update the existing challengeProgress document
+        challengeProgressDoc = challengeProgressSnapshot.docs[0];
+        if (challengeProgressDoc.data().isCompleted) {
+          console.log("Challenge progress is already completed:", challengeID);
+          continue;
+        }
+        const currentCount = challengeProgressDoc.data().progress.count;
+        const newCount = currentCount + 1;
+        const isCompleted = newCount >= requiredCount;
+
+        await challengeProgressDoc.ref.update({
+          "progress.count": newCount,
+          isCompleted,
+        });
+        console.log(
+          "Updated challengeProgress document:",
+          challengeProgressDoc.id,
+          "newCount:",
+          newCount,
+          "isCompleted:",
+          isCompleted
+        );
+      }
+
+      // Update coins field in userPoints or leagueStats
+      if (leagueID) {
+        console.log(
+          "Updating leagueStats for user:",
+          userID,
+          "leagueID:",
+          leagueID
+        );
+        console.log(
+          "Querying leagueStats with userId:",
+          userID,
+          "and leagueId:",
+          leagueID
+        );
+        const leagueStatsRef = admin
+          .firestore()
+          .collection("leagueStats")
+          .where("userId", "==", userID)
+          .where("leagueId", "==", leagueID);
+        const leagueStatsSnapshot = await leagueStatsRef.get();
+        console.log("leagueStatsSnapshot size:", leagueStatsSnapshot.size);
+
+        leagueStatsSnapshot.forEach((doc) => {
+          console.log("leagueStats document: >>", doc.id, doc.data());
+        });
+
+        if (!leagueStatsSnapshot.empty) {
+          const leagueStatsDoc = leagueStatsSnapshot.docs[0];
+
+          await leagueStatsDoc.ref.update({
+            coins: admin.firestore.FieldValue.increment(200),
+          });
+          console.log(
+            "Updated leagueStats for user:",
+            userID,
+            "leagueID:",
+            leagueID
+          );
+        } else {
+          console.log(
+            "No leagueStats document found for user:",
+            userID,
+            "leagueID:",
+            leagueID
+          );
+        }
+      } else {
+        console.log("Updating userPoints for user:", userID);
+        const userPointsRef = admin
+          .firestore()
+          .collection("userPoints")
+          .doc(userID);
+        await userPointsRef.update({
+          coins: admin.firestore.FieldValue.increment(200),
+        });
+        console.log("Updated userPoints for user:", userID);
+      }
+    }
+
+    return { success: true, message: "Building creation handled successfully" };
+  } catch (error) {
+    console.error("Error handling building creation:", error);
+    return { success: false, message: error.message };
+  }
+}
+
+router.post("/create-challenge-progress", checkAuth, async (req, res) => {
+  const { challengeID, userID, buildingID, count } = req.body;
+
+  if (!challengeID || !userID || !buildingID || !count) {
+    return res
+      .status(400)
+      .json(createResponse(false, "All fields are required"));
+  }
+
+  const challengeProgress = {
+    challengeID,
+    userID,
+    progress: {
+      buildingID,
+      count,
+    },
+    isCompleted: false,
+  };
+
+  try {
+    const challengeRef = admin
+      .firestore()
+      .collection("challenges")
+      .doc(challengeID);
+    const challengeDoc = await challengeRef.get();
+    if (!challengeDoc.exists) {
+      return res
+        .status(404)
+        .json(
+          createResponse(false, `No challenge found for the id: ${challengeID}`)
+        );
+    }
+
+    await admin
+      .firestore()
+      .collection("challengeProgress")
+      .add(challengeProgress);
+    res.json(createResponse(true, "Challenge progress created successfully"));
+  } catch (error) {
+    console.error("Error creating challenge progress:", error);
+    res
+      .status(500)
+      .json(createResponse(false, "An error occurred", error.message));
+  }
+});
+
+router.get("/active-challenges", checkAuth, async (req, res) => {
+  try {
+    const now = new Date();
+    const activeChallengesSnapshot = await admin
+      .firestore()
+      .collection("challenges")
+      .where("isEnded", "==", false)
+      .get();
+
+    const activeChallenges = activeChallengesSnapshot.docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
     }));
 
-    console.log("Buildings:", buildings);
-
-    // Calculate the destruction summary grouped by user ID and building type
-    const destructionMap = {};
-    randomAssets.forEach((asset) => {
-      const building = buildings.find(
-        (b) => String(b.id) === String(asset.buildingId)
-      );
-      if (building) {
-        const title = building.title.toLowerCase();
-        const normalizedTitle = title.replace(/house[a-z]*$/, "house");
-        if (!destructionMap[asset.userId]) {
-          destructionMap[asset.userId] = {};
-        }
-        if (!destructionMap[asset.userId][normalizedTitle]) {
-          destructionMap[asset.userId][normalizedTitle] = 0;
-        }
-        destructionMap[asset.userId][normalizedTitle] += 1;
-      } else {
-        console.log(
-          `No building found for asset with buildingId: ${asset.buildingId}`
-        );
-      }
-    });
-
-    console.log("Destruction Map:", destructionMap);
-
-    // Select a random disaster from the disasters array
-    const randomDisasterIndex = Math.floor(Math.random() * disasters.length);
-    const randomDisaster = disasters[randomDisasterIndex];
-
-    // Create the disaster response object
-    const disasterResponse = Object.entries(destructionMap).map(
-      ([userId, destruction]) => ({
-        disaster: randomDisaster,
-        destruction: destruction,
-        userId: userId,
-      })
-    );
-
-    // Send the response with the destruction summary and the random disaster
     res.json(
       createResponse(
         true,
-        `Destruction caused by ${randomDisaster}`,
-        disasterResponse
+        "Active challenges fetched successfully",
+        activeChallenges
       )
     );
   } catch (error) {
-    // Handle any errors that occur during the process
-    console.error("Error getting random disaster:", error);
+    console.error("Error fetching active challenges:", error);
+    res
+      .status(500)
+      .json(createResponse(false, "An error occurred", error.message));
+  }
+});
+
+router.get("/completed-challenges", checkAuth, async (req, res) => {
+  const userID = req.user.user_id;
+  const { leagueID } = req.query;
+
+  try {
+    let query = admin
+      .firestore()
+      .collection("challengeProgress")
+      .where("userID", "==", userID)
+      .where("isCompleted", "==", true);
+
+    if (leagueID) {
+      query = query.where("leagueID", "==", leagueID);
+    } else {
+      query = query.where("leagueID", "==", null);
+    }
+
+    const completedChallengesSnapshot = await query.get();
+    const completedChallenges = completedChallengesSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    res.json(
+      createResponse(
+        true,
+        "Completed challenges fetched successfully",
+        completedChallenges
+      )
+    );
+  } catch (error) {
+    console.error("Error fetching completed challenges:", error);
     res
       .status(500)
       .json(createResponse(false, "An error occurred", error.message));
